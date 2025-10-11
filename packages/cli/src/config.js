1e9ed950f5a550c2b91c0be37bb5a443799ab63f
John Gilbert <john.gilbert@devtechnology.com>
2025-10-11 00:10:34 -0400

    apigw

--- packages/cli/src/config.js
+++ packages/cli/src/config.js
@@ -6,20 +6,20 @@ import {
   resolveFromAws,
   resolveFromAwsDesc,
   resolveFromCf,
-  // TODO standardResolvers ???
   ParsePlugin,
 } from '@convivio/parse';
 import {
   cicdCredentials,
 } from '@convivio/connectors';
 import {
+  AlbPlugin,
+  ApiGatewayPlugin,
   CorePlugin,
   LambdaPlugin,
   ResourcesPlugin,
   StreamPlugin,
   SqsPlugin,
   SchedulePlugin,
-  // TODO standardGenerators ???
 } from '@convivio/gen';
 import {
   DeployPlugin,
@@ -27,8 +27,6 @@ import {
   SecretsManagerPlugin,
 } from '@convivio/deploy';
 import { WebpackPlugin } from '@convivio/webpack';
-
-// TODO standard resolvers/plugins presets
 
 import TracePlugin from './trace';
 
@@ -46,33 +44,22 @@ export default (convivio, overrides) => ({
     env: resolveFromObject(process.env),
     self: resolveFromSelf(convivio),
     param: resolveFromParam(convivio),
-    // sls: resolveFromSls(convivio),
     file: resolveFromFile(process.cwd()),
     aws: resolveFromAws(convivio),
     awsdesc: resolveFromAwsDesc(convivio),
     cf: resolveFromCf(convivio),
-    // s3: resolveFromCf(convivio),
-    // ssm: resolveFromCf(convivio),
 
-    // ...standardResolvers(convivio),
     ...(overrides?.resolvers || {}),
   },
   plugins: overrides?.plugins || [
     new TracePlugin(convivio.options),
     new ParsePlugin(convivio.options),
 
-    // ...standardGenPlugins(convivio),
     new CorePlugin(convivio.options),
     new LambdaPlugin(convivio.options),
 
-    // TODO events
-    //  api gateway
-    //  alb
-    //  sqs
-    //  sns ???
-    //  cloudwatch / schedule*2
-    //  s3 ???
-    //  kafka
+    new AlbPlugin(convivio.options),
+    new ApiGatewayPlugin(convivio.options),
 
     new StreamPlugin(convivio.options),
     new SqsPlugin(convivio.options),
@@ -80,7 +67,6 @@ export default (convivio, overrides) => ({
 
     new ResourcesPlugin(convivio.options),
 
-    // TODO swap packaging ???
     new WebpackPlugin(convivio.options),
 
     new DeployPlugin(convivio.options),
